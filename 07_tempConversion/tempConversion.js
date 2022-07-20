7c489b41c4fb1243553955379cef271bb41347a9
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-07-20 19:22:32 +0530

    complete 06_07_Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let celsius = ((temp - 32) * 5) / 9;
+  var rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let far = (temp * 9) / 5 + 32;
+  var rounded = Math.round(far * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
