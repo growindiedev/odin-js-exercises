89f8deab3618765126293462ea8ff65e4d6298ad
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-07-20 16:18:14 +0530

    complete 04_exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // let whoop = arr.filter((elm) => !args.includes(elm));
+  let whoop = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    if (!args.includes(arr[i])) {
+      whoop.push(arr[i]);
+    }
+  }
+  console.log("before$", arr);
+  console.log("btw$", args);
+  console.log("after$", whoop);
+  return whoop;
 };
 
 // Do not edit below this line
