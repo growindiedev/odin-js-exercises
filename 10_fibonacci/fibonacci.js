eec22212b48c555f9c04256c8b6470f4ac59e905
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-07-21 07:39:40 +0530

    complete 10_exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  // handling last value is the key
+  // break the input into two parts => second last value + last value
+  //watch cs dojo video
+  num = Number(num);
+  if (num < 0 || typeof num !== "number") {
+    return "OOPS";
+  } else if (num <= 1) {
+    return num;
+  }
+  return fibonacci(num - 2) + fibonacci(num - 1);
 };
 
 // Do not edit below this line
