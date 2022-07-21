528b54561765f3a13538fece2328290f77d74c68
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-07-21 09:04:43 +0530

    finish 02_exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const d = new Date();
+  let year = d.getFullYear();
+  //let oldest = people[0];
 
+  let oldestPerson = people.reduce((previousPerson, current) => {
+    let oldest = previousPerson;
+    let age = (current.yearOfDeath || year) - current.yearOfBirth;
+    let oldestAge = (oldest.yearOfDeath || year) - oldest.yearOfBirth;
+    if (age >= oldestAge) {
+      oldest = current;
+    }
+    return oldest;
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
